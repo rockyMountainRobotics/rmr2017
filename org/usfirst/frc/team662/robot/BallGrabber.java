07ed27667a940ef825ce30dc9f9a3dd1efe47fa4
jkrmnj <jkrmnj465@gmail.com>
2017-02-13 19:39:27 -0700

    Added Ball Grabber working

--- org/usfirst/frc/team662/robot/BallGrabber.java
+++ org/usfirst/frc/team662/robot/BallGrabber.java
@@ -1,21 +1,35 @@
 package org.usfirst.frc.team662.robot;
 import com.ctre.CANTalon;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SpeedController;
 
 public class BallGrabber implements Component{
 
+	//FIND WHAT PORT WE ARE SUPPOSED TO USE!!!
 	final static int MANIPULATOR_MOTOR_PORT = 0;
+	final static double GRAB_DEADZONE = 0.1;
 	CANTalon ballGrab = new CANTalon(1);
+	Solenoid ballPlace = new Solenoid(2);
 
 	
 	@Override
 	public void update(){
-		//Uses the right trigger
-		Robot.stick.getRawButton(XboxMap.RIGHT_TRIGGER);
-		//If the right analog button is pressed, the ball grabber motor moves at .25, obviously.
-		if(Robot.stick.getRawButton(XboxMap.R_ANALOG) == true){
-			ballGrab.set(.25);
+	
+		//set the ball collector speed to the value of the right trigger
+		if(Robot.stick.getRawAxis(XboxMap.RIGHT_TRIGGER) > GRAB_DEADZONE){
+			ballGrab.set(Robot.stick.getRawAxis(XboxMap.RIGHT_TRIGGER));
+		}
+		else{
+			ballGrab.set(0);
+		}
+		
+		//if the b button is held, open the ball holder. Otherwise, close it
+		if(Robot.stick.getRawButton(XboxMap.B)){
+			ballPlace.set(true);
+		}
+		else{
+			ballPlace.set(false);
 		}
 	}
 	@Override
@@ -25,5 +39,6 @@ public class BallGrabber implements Component{
 	@Override
 	public void disable() {
 		ballGrab.set(0);
+		ballPlace.set(false);
 	}
 }
